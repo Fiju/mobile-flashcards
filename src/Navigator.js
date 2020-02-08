e6555531052a9f5240c4117ba14342afec1f78e0
Fiju <faixanshirax@gmail.com>
2020-02-09 03:53:06 +0500

    feat: Tab added for add new deck

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DeckList from "./screens/DeckList";
 import DeckDetails from "./screens/DeckDetails";
+import AddDeck from "./screens/AddDeck";
 
 const Stack = createStackNavigator();
 
-export default function MyStack() {
+function HomeStackScreen() {
   return (
     <Stack.Navigator initialRouteName="DeckList">
       <Stack.Screen name="DeckList" component={DeckList} />
       <Stack.Screen name="DeckDetails" component={DeckDetails} />
     </Stack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function MyStack() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStackScreen} />
+      <Tab.Screen name="AddDeck" component={AddDeck} />
+    </Tab.Navigator>
   );
 }
